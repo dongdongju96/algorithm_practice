7aa6484e79324de04cfd0777acdbfcbb5f617379
dongdongju96 <leedongju15@gmail.com>
2022-03-12 21:11:09 +0900

    Add answer

--- etc/baekjoon_9012.py
+++ etc/baekjoon_9012.py
@@ -1,21 +1,39 @@
 import sys
 
+# def vsp_check(str):
+#     stack = []
+#     result = ''
+#     for parenthesis in str:
+#         if parenthesis=='(':
+#             stack.append(parenthesis)
+#             result = 'NO'
+#         if parenthesis==')':          
+#             if len(stack)>0:
+#                 stack.pop()
+#                 if len(stack)==0:
+#                     result = 'YES'
+#                 else:
+#                     result = 'NO'
+#             else:
+#                 result='NO'
+
+#     return result
+
 def vsp_check(str):
-    stack = []
+    sum = 0
     result = ''
     for parenthesis in str:
         if parenthesis=='(':
-            stack.append(parenthesis)
-            result = 'NO'
-        if parenthesis==')':          
-            if len(stack)>0:
-                stack.pop()
-                if len(stack)==0:
-                    result = 'YES'
-                else:
-                    result = 'NO'
-            else:
-                result='NO'
+            sum += 1
+        elif parenthesis==')':
+            sum += -1
+        if sum < 0:    ## 놓친 포인트
+            break
+        
+    if sum==0:
+        result = 'YES'
+    else:
+        result = 'NO'
 
     return result
 
