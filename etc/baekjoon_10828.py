52ca6f11b115a38f5e3f81d08a0f47e741a62450
dongdongju96 <leedongju15@gmail.com>
2022-03-12 13:13:29 +0900

    Add answer

--- etc/baekjoon_10828.py
+++ etc/baekjoon_10828.py
@@ -24,10 +24,12 @@ def top(input_list):
     else:
         print(-1)
 
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 stack = []
 for _ in range(n):
-    input_val = input().split()
+    input_val = sys.stdin.readline().split()
     if input_val[0]=='push':
         push(stack,input_val[1])
     elif input_val[0]=='pop':
@@ -38,3 +40,7 @@ for _ in range(n):
         empty(stack)
     else:
         top(stack)
+
+
+# 첫 번째 시도에 에러가 난 이유는 input()을 사용해서 그렇다
+# sys.stdin.readline()을 사용하면 된다.
