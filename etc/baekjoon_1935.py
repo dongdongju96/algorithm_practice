af41198b5a7a2fdd138f8e0bd640ac2b57af6556
dongdongju96 <leedongju15@gmail.com>
2022-03-25 20:10:45 +0900

    Add answer

--- etc/baekjoon_1935.py
+++ etc/baekjoon_1935.py
@@ -7,27 +7,21 @@ for number in range(n):
     number_list.append(int(input()))
 
 for i in input_str:
-    if i=='+':
-        number_1 = stack.pop()
-        number_2 = stack.pop()
-        stack.append(number_1 + number_2)
-    
-    elif i=='-':
-        number_1 = stack.pop()
-        number_2 = stack.pop()
-        stack.append(number_1 - number_2)
-        
-    elif i=='*':
-        number_1 = stack.pop()
-        number_2 = stack.pop()
-        stack.append(number_1 * number_2)
-
-    elif i=='/':
-        number_1 = stack.pop()
-        number_2 = stack.pop()
-        stack.append(number_1 / number_2)
+    if i >= 'A' and i <= 'Z':
+        stack.append(number_list[ord(i)-ord('A')])
 
     else:
-        stack.append(number_list[ord(i)-ord('A')])
+        number_2 = stack.pop()
+        number_1 = stack.pop()
+        
+        
+        if i=='+':
+            stack.append(number_1 + number_2)
+        elif i=='-':
+            stack.append(number_1 - number_2)
+        elif i=='*':
+            stack.append(number_1 * number_2)
+        elif i=='/':
+            stack.append(number_1 / number_2)
     
-print(stack[-1])
+print('%.2f' %stack[-1])
