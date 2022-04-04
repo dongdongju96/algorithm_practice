10319da07f4ead19ebb7c7e5cbaa55379ef75c36
dongdongju96 <leedongju15@gmail.com>
2022-04-04 21:21:22 +0900

    Add another person answer

--- etc/baekjoon_11655.py
+++ etc/baekjoon_11655.py
@@ -19,3 +19,15 @@ for char in input_text:
 for i in stack:
     print(i,end='')
 
+# 다른 사람의 풀이
+
+# s = input()
+# res = ''
+# for c in s:
+#     if 'a' <= c <= 'z':
+#         res += chr((ord(c)+13) if c <= 'm' else ord(c)-13)
+#     elif 'A' <= c <= 'Z':
+#         res += chr((ord(c)+13) if c <= 'M' else ord(c)-13)
+#     else:
+#         res += c
+# print(res)
