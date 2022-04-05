65b1b34f0c4e0121301301b4f38181cfe5d7787a
dongdongju96 <leedongju15@gmail.com>
2022-04-05 22:43:08 +0900

    Add wrong answer

---
+++ etc/baekjoon_10820.py
@@ -0,0 +1,14 @@
+str = input()
+stack = [0]*4
+
+for s in str:
+    print(s)
+    if ord(s)>=ord('a') and ord(s)<=ord('z'):
+        stack[0] = stack[0] + 1
+    elif ord(s)>=ord('A') and ord(s)<=ord('Z'):
+        stack[1] = stack[1] + 1
+    elif ord(s)>=ord('0') and ord(s)<=ord('9'):
+        stack[2] = stack[2] + 1
+    elif ord(s)==ord(' '):
+        stack[3] = stack[3] + 1
+print(*stack)
