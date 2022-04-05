929e649ee86397b937ec11c18a7fc196fd1123af
dongdongju96 <leedongju15@gmail.com>
2022-04-05 22:57:32 +0900

    Add answer

--- etc/baekjoon_10820.py
+++ etc/baekjoon_10820.py
@@ -1,14 +1,14 @@
-str = input().rstrip('\n')
-print(str)
-stack = [0]*4
+str_list = list(input().split('\n'))
 
-for s in str:
-    if ord(s)>=ord('a') and ord(s)<=ord('z'):
-        stack[0] = stack[0] + 1
-    elif ord(s)>=ord('A') and ord(s)<=ord('Z'):
-        stack[1] = stack[1] + 1
-    elif ord(s)>=ord('0') and ord(s)<=ord('9'):
-        stack[2] = stack[2] + 1
-    elif ord(s)==ord(' '):
-        stack[3] = stack[3] + 1
-print(*stack)
+for str in str_list:
+    stack = [0]*4
+    for s in str:
+        if ord(s)>=ord('a') and ord(s)<=ord('z'):
+            stack[0] = stack[0] + 1
+        elif ord(s)>=ord('A') and ord(s)<=ord('Z'):
+            stack[1] = stack[1] + 1
+        elif ord(s)>=ord('0') and ord(s)<=ord('9'):
+            stack[2] = stack[2] + 1
+        elif ord(s)==ord(' '):
+            stack[3] = stack[3] + 1
+    print(*stack, end='\n')
